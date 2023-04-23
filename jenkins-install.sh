750dbe23a8c56c18dd5e6b76000a006544c255fa
awanmbandi <68437700+awanmbandi@users.noreply.github.com>
2023-04-23 09:47:37 -0400

    Update jenkins-install.sh

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -2,7 +2,7 @@
 sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
+sudo yum upgrade -y
 sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
