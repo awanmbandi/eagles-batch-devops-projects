031ab99f534328f45619928343d623161f447865
clovette <113598817+clovette@users.noreply.github.com>
2023-06-07 20:46:31 -0400

    Update jenkins-install.sh

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
