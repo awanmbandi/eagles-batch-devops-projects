23b75a076e76b4d5809d858e2c1eb2f1848083ee
Gabin Sime <115108532+Gabinsime75@users.noreply.github.com>
2023-07-23 22:32:11 -0700

    Update install-grafana.sh

--- install-grafana.sh
+++ install-grafana.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y adduser libfontconfig1
-wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
-sudo dpkg -i grafana_7.3.4_amd64.deb
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_10.0.2_amd64.deb
+sudo dpkg -i grafana-enterprise_10.0.2_amd64.deb
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
